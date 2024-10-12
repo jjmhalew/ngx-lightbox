055ce191269af830e7c18dc7663eb8455796afd0
Jim <38460550+jjmhalew@users.noreply.github.com>
2024-10-13 01:34:54 +0200

    fix build

--- projects/ngx-lightbox/src/public-api.ts
+++ projects/ngx-lightbox/src/public-api.ts
@@ -2,5 +2,9 @@
  * Public API Surface of ngx-lightbox
  */
 
-// export * from './lib/ngx-lightbox.service';
-// export * from './lib/ngx-lightbox.component';
+export { Lightbox } from "./lib/services/lightbox.service";
+export { LightboxConfig } from "./lib/services/lightbox-config.service";
+export { LightboxEvent, LIGHTBOX_EVENT } from "./lib/services/lightbox-event.service";
+export type { IAlbum, IEvent } from "./lib/services/lightbox-event.service";
+
+export { LightboxModule } from "./lib/lightbox.module";
